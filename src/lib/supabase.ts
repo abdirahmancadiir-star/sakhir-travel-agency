7876f70e36602abb0b63972a27eecc7293d28303
abdirahmancadiir-star <abdirahmancadiir@gmail.com>
2026-06-10 00:04:25 +0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,11 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://zgnpgsqpgahjwxvjeeio.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjYWtlamxsd2N6YnNxdWhwd2NwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwMjQ5MTcsImV4cCI6MjA5NjYwMDkxN30.lcgRGvBqsdJ58I-70gM5Ke46o-YM4URjF2Udu9qHSdA'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    persistSession: true,
-    detectSessionInUrl: true,
-  },
-})
+export const supabase = createClient(
+  'https://acakejllwczbsquhpwcp.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjYWtlamxsd2N6YnNxdWhwd2NwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwMjQ5MTcsImV4cCI6MjA5NjYwMDkxN30.lcgRGvBqsdJ58I-70gM5Ke46o-YM4URjF2Udu9qHSdA'
+)
