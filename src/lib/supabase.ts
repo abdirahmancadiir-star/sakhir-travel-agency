523f93cea32641b313fca4f52a93103dfb6789c3
abdirahmancadiir-star <abdirahmancadiir@gmail.com>
2026-06-09 23:40:32 +0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://zgnpgsqpgahjwxvjeeio.supabase.co'
-const supabaseAnonKey = 'ANON_KEY_GAAGA'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjYWtlamxsd2N6YnNxdWhwd2NwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwMjQ5MTcsImV4cCI6MjA5NjYwMDkxN30.lcgRGvBqsdJ58I-70gM5Ke46o-YM4URjF2Udu9qHSdA'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
